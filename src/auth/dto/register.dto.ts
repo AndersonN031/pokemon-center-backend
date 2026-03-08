9ca80dc3060b27e8bbc6745543d9641bc8f69cf8
AndersonN031 <devandersonnunes078@gmail.com>
2026-03-08 15:06:23 -0300

    Adicionando validação no register.dto e create-user.dto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,26 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsStrongPassword, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { INVALID_CREDENTIALS, SPECIAL_CHARACTERS } from '../auth.constants';
 
 export class RegisterDto {
-  @ApiProperty({title: "Name", example: "John Doe"})
+  @ApiProperty({ title: 'Name', example: 'John Doe' })
   @IsString()
+  @MinLength(2, { message: 'Minimo 2 caracteres' })
+  @MaxLength(100, { message: 'Máximo 100 caracteres' })
   name: string;
 
-  @ApiProperty({title: "Email", example: "john.doe@example.com"})
+  @ApiProperty({ title: 'Email', example: 'john.doe@example.com' })
   @IsEmail()
+  @MaxLength(254, {message: "Máximo 254 caracteres"})
   email: string;
 
-  @ApiProperty({title: "Password", example: "JohnDoe@123"})
+  @ApiProperty({ title: 'Password', example: 'JohnDoe@123' })
+   @MaxLength(128, {message: "Máximo 128 caracteres"})
   @IsStrongPassword(
-		{
-			minLength: 6,
-			minLowercase: 1,
-			minUppercase: 1,
-			minNumbers: 1,
-			minSymbols: 1,
-		},
-		{ message: SPECIAL_CHARACTERS },
-	)
-	password: string;
+    {
+      minLength: 6,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    { message: SPECIAL_CHARACTERS },
+  )
+  password: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @MinLength(2, {message: 'Minimo 2 caracteres'})
+  @MaxLength(100, {message: "Máximo 100 caracteres"})
   name: string;
 
   @IsEmail()
+  @MaxLength(254, {message:"Máximo 254 caracteres"})
   email: string;
 
-  @MinLength(6)
+  @MinLength(6, {message: "Minimo 6 caracteres"})
+   @MaxLength(128, {message: "Máximo 128 caracteres"})
   password: string;
 }
